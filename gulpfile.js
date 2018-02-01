04edb2d551d3a58100b04a1c36c24335e7213683
Rogatnev Nikita <nikita.rogatnev@omnigon.com>
2018-02-01 04:10:37 +0300

    init

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,26 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
-gulp.task("style", function() {
-  gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+gulp.task("style", function () {
+    gulp.src("source/less/style.less")
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest("source/css"))
+        .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
+gulp.task("serve", ["style"], function () {
+    server.init({
+        server: "source/",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
 
-  gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+    gulp.watch("source/less/**/*.less", ["style"]);
+    gulp.watch("source/*.html").on("change", server.reload);
 });
