3e5134c111ec7a3b88ed764729e334fe74620b3e
Nikita Rogatnev <tools@numatic.co>
2023-02-04 18:48:59 +0400

    certifications 2023

--- src/components/certificates/certificates.jsx
+++ src/components/certificates/certificates.jsx
@@ -12,76 +12,65 @@ class Certificates extends React.PureComponent {
                     <div className="certificates__content">
                         <h2 className="title">Certifications</h2>
                         <ul className="certificates__items">
-                            <li className="certificates__item">2023 - Semrush for Digital Agencies Certification</li>
-                            <li className="certificates__item">2023 - Semrush SEO Toolkit Course</li>
+                            <li className="certificates__item">2023 - Professional Scrum Master™ I (PSM I), Scrum.org</li>
+                            <li className="certificates__item">2023 - LinkedIn Certified Marketing Insider, LinkedIn</li>
+                            <li className="certificates__item">2023 - LinkedIn Content and Creative Design, LinkedIn</li>
+                            <li className="certificates__item">2023 - LinkedIn Marketing Strategy, LinkedIn</li>
+                            <li className="certificates__item">2023 - LinkedIn Marketing Solutions Fundamentals, LinkedIn</li>
+                            <li className="certificates__item">2023 - Google Analytics Certification, Google</li>
+                            <li className="certificates__item">2023 - Google Ads Search Certification, Google</li>
+                            <li className="certificates__item">2023 - Semrush for Digital Agencies Certification, Semrush</li>
+                            <li className="certificates__item">2023 - Semrush SEO Toolkit Course, Semrush</li>
                             <li className="certificates__item">2021 - Yandex.Direct Expert, Yandex</li>
                             <li className="certificates__item">2021 - The Complete Finance Manager Course 2021, Udemy</li>
                             <li className="certificates__item">2020 - Apple Search Ads, Apple</li>
                             <li className="certificates__item">2020 - The Complete Salesforce Development Course, Udemy</li>
                             <li className="certificates__item">2020 - Google Ads Search Certification, Google</li>
                             <li className="certificates__item">2020 - Divemaster (DM), PADI</li>
-                            <li className="certificates__item">2019 - Primary & Secondary Care CPR / First Aid, PADI
-                            </li>
+                            <li className="certificates__item">2019 - Primary & Secondary Care CPR / First Aid, PADI</li>
                             <li className="certificates__item">2019 - Rescue Diver, PADI</li>
                             <li className="certificates__item">2019 - Web Designer Basics, Google</li>
                             <li className="certificates__item">2019 - The Fundamentals of Digital Marketing, Google</li>
                             <li className="certificates__item">2019 - JavaScript Level 3, HTML Academy</li>
                             <li className="certificates__item">2019 - JavaScript Level 2, HTML Academy</li>
-                            <li className="certificates__item">2019 - Understanding Information Control For IT Managers
-                                Certificate,
-                                Alison
-                            </li>
+                            <li className="certificates__item">2019 - Understanding Information Control For IT Managers Certificate, Alison</li>
                             <li className="certificates__item">2019 - Digital Sales Certification, Google</li>
                             <li className="certificates__item">2019 - Creative Certification Exam, Google</li>
-                            <li className="certificates__item">2019 - Google Analytics Individual Qualification,
-                                Google
-                            </li>
-                            <li className="certificates__item">2019 - Software Processes and Agile Practices, Coursera
-                            </li>
-                            <li className="certificates__item">2019 - Reviews & Metrics for Software Improvements,
-                                Coursera
-                            </li>
-                            <li className="certificates__item">2019 - Introduction to Software Product Management,
-                                Coursera
-                            </li>
-                            <li className="certificates__item">2019 - Creating a Team Culture of Continuous Learning,
-                                Coursera
-                            </li>
-                            <li className="certificates__item">2016 - Google AdWords Certification - Shopping
-                                Advertising, Google
-                            </li>
-                            <li className="certificates__item">2016 - Google Analytics Individual Qualification,
-                                Google
-                            </li>
+                            <li className="certificates__item">2019 - Google Analytics Individual Qualification, Google</li>
+                            <li className="certificates__item">2019 - Software Processes and Agile Practices, Coursera</li>
+                            <li className="certificates__item">2019 - Reviews & Metrics for Software Improvements, Coursera</li>
+                            <li className="certificates__item">2019 - Introduction to Software Product Management, Coursera</li>
+                            <li className="certificates__item">2019 - Creating a Team Culture of Continuous Learning, Coursera</li>
+                            <li className="certificates__item">2016 - Google AdWords Certification - Shopping Advertising, Google</li>
+                            <li className="certificates__item">2016 - Google Analytics Individual Qualification, Google</li>
                             <li className="certificates__item">2016 - Yandex Direct Certified Professional, Yandex</li>
-
-                            <li className="certificates__item">2015 - Fundamentals Of Network Security Certificate,
-                                Alison
-                            </li>
+                            <li className="certificates__item">2015 - Fundamentals Of Network Security Certificate,Alison</li>
                             <li className="certificates__item">2015 - CCNA 3 - Scaling Networks, Cisco</li>
                             <li className="certificates__item">2015 - CCNA 4 - Connecting networks, Cisco</li>
-                            <li className="certificates__item">2015 - MTA: Windows Operating System Fundamentals,
-                                Microsoft
-                            </li>
-                            <li className="certificates__item">2014 - MTA: HTML5 Application Development Fundamentals,
-                                Microsoft
-                            </li>
-                            <li className="certificates__item">2014 - Data Center University Associate Development Path,
-                                Schneider Electric
-                            </li>
-                            <li className="certificates__item">2014 - MCP: Microsoft Certified Professional, Microsoft
-                            </li>
-                            <li className="certificates__item">2014 - CCNA 2 - Routing and Switching Essentials, Cisco
-                            </li>
+                            <li className="certificates__item">2015 - MTA: Windows Operating System Fundamentals, Microsoft</li>
+                            <li className="certificates__item">2014 - MTA: HTML5 Application Development Fundamentals, Microsoft</li>
+                            <li className="certificates__item">2014 - Data Center University Associate Development Path, Schneider Electric</li>
+                            <li className="certificates__item">2014 - MCP: Microsoft Certified Professional, Microsoft</li>
+                            <li className="certificates__item">2014 - CCNA 2 - Routing and Switching Essentials, Cisco</li>
                             <li className="certificates__item">2013 - CCNA 1 - Introduction to Networks, Cisco</li>
                             <li className="certificates__item">2013 - PHP Developer Certificate, W3Schools</li>
                             <li className="certificates__item">2012 - Information Storage and Management, EMC</li>
-                            <li className="certificates__item">2012 - Master of digital information processing, GBOU
-                                liceum at SPB State University of Telecommunications
-                            </li>
-                            <li className="certificates__item">2011 - Internet and Computing Core Certification, Basis
-                            </li>
+                            <li className="certificates__item">2012 - Master of digital information processing, GBOU liceum at SPB State University of Telecommunications</li>
+                            <li className="certificates__item">2011 - Internet and Computing Core Certification, Basis</li>
                         </ul>
+                        <div className="certificates__badges">
+                          <a href="https://www.scrum.org/certificates/915285" targt="_blank"><img src="./img/badges/psm-1.png" alt="scrum" height="175" className="certificates__badges-item" /></a>
+                          <a targt="_blanc"><img src="./img/badges/HubSpot.png" alt="HubSpot" height="175" className="certificates__badges-item" /></a>
+                          <a href="https://verify.skilljar.com/c/o5fm8k4z83yv" targt="_blanc"><img src="./img/badges/LinkedIn.png" alt="LinkedIn" height="175" className="certificates__badges-item" /></a>
+                          <a href="https://skillshop.credential.net/bfdcfc87-d828-49ab-b685-883339278113?record_view=true" targt="_blanc"><img src="./img/badges/google-ads.png" alt="google" height="175" className="certificates__badges-item" /></a>
+                          <a href="https://skillshop.credential.net/31e694ce-2736-4ba9-8759-3d5a04bd3f35?record_view=true" targt="_blanc"><img src="./img/badges/google-analytics.png" alt="google" height="175" className="certificates__badges-item" /></a>
+
+                          <a targt="_blanc"><img src="./img/badges/yandex-partner.png" alt="yandex" height="175" className="certificates__badges-item" /></a>
+                          <a targt="_blanc"><img src="./img/badges/mta-badge.png" alt="mta" height="175" className="certificates__badges-item" /></a>
+                          <a targt="_blanc"><img src="./img/badges/MCP-badge.png" alt="MCP" height="175" className="certificates__badges-item" /></a>
+                          <a targt="_blanc"><img src="./img/badges/ccna.png" alt="ccna" height="175" className="certificates__badges-item" /></a>
+                          <a href="https://www.semrush.com/agencies/numatic-ventures/" alt="semrush" target="_blank"><img src="https://static.semrush.com/agency-directory/shared/badge.svg" alt="Semrush certified agency partner badge"/></a>
+                        </div>
                     </div>
                 </div>
             </section>
